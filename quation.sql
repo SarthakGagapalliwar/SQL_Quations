567c80604a2321784d16fc438a6170229d5b0cd9
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2025-08-12 19:00:39 +0530

    sql-quation-day1

---
+++ quation.sql
@@ -0,0 +1,36 @@
+-- Show first name, last name, and gender of patients whose gender is 'M'
+SELECT first_name,last_name,gender 
+from patients 
+where gender ='M'
+
+
+-- Show first name and last name of patients who does not have allergies. (null)
+SELECT first_name,last_name
+from patients 
+where allergies is NULL;
+
+
+-- Show first name of patients that start with the letter 'C'
+SELECT first_name,
+from patients 
+where first_name LIKE 'C%';
+
+
+-- Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
+SELECT first_name,last_name
+from patients 
+where weight<=120 AND weight>=100;
+
+
+-- Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
+update patients
+SET allergies='NKA'
+where allergies is null;
+
+
+-- Show first name and last name concatinated into one column to show their full name.
+select concat (first_name, ' ', last_name) As full_name
+FROM patients
+
+
+
---
+++ quation3.sql
@@ -0,0 +1,42 @@
+-- Show unique birth years from patients and order them by ascending.
+select distinct year(birth_date) as birth_year
+from patients
+order by birth_year;
+
+-- Show unique first names from the patients table which only occurs once in the list.
+-- For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. 
+-- If only 1 person is named 'Leo' then include them in the output.
+select first_name
+from patients
+group by first_name
+having count(*)=1;
+
+
+
+
+-- Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+select patient_id,first_name
+from patients
+where first_name like 's____%s';
+
+
+SELECT patient_id, first_name
+FROM patients
+WHERE first_name LIKE 's%s'; --with all starts with s and end with s;
+
+
+-- Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
+-- Primary diagnosis is stored in the admissions table.
+select patients.patient_id,first_name,last_name
+From patients
+join admissions on patients.patient_id=admissions.patient_id
+where diagnosis='Dementia'
+
+
+-- Display every patient's first_name.
+-- Order the list by the length of each name and then by alphabetically.
+select first_name
+from patients
+order by len(first_name),first_name
+
+
